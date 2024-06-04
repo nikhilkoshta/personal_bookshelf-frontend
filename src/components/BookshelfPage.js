1c8cb35a043937cc093d4b0d19b142153a0665ee
Nikhil <72139039+nikhilkoshta@users.noreply.github.com>
2024-06-04 19:57:23 +0530

    Update BookshelfPage.js

--- src/components/BookshelfPage.js
+++ src/components/BookshelfPage.js
@@ -6,7 +6,7 @@ const BookshelfPage = () => {
   const [bookshelf, setBookshelf] = useState([]);
 
   useEffect(() => {
-    axios.get('https://personal-bookshelf-frontend.vercel.app/books')
+    axios.get('https://personal-bookshelf-backend.vercel.app/books')
       .then(response => {
         setBookshelf(response.data);
       })
